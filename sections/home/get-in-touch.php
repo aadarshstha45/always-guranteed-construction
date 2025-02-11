c479ee332f8909ab20a2c45d3774556687a58ba2
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2025-02-11 18:18:47 +0545

    font-path

--- sections/home/get-in-touch.php
+++ sections/home/get-in-touch.php
@@ -1,5 +1,5 @@
 <section
-  class="w-full bg-[url(<?= base_URL ?>/assets/images/contact-bg.png)] bg-cover bg-center bg-no-repeat relative min-h-[200px] py-[115px] px-[10px] min-[450px]:px-[50px] min-[1300px]:px-[135px]">
+  class="w-full bg-[url(<?= BASE_URL ?>/assets/images/contact-bg.png)] bg-cover bg-center bg-no-repeat relative min-h-[200px] py-[115px] px-[10px] min-[450px]:px-[50px] min-[1300px]:px-[135px]">
   <div class="absolute bg-black/70 top-0 left-0 w-full h-full"></div>
   <div
     class="grid grid-cols-1 min-[1060px]:grid-cols-2 items-center gap-12 min-[1060px]:gap-4 relative">
